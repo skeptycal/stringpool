33c056a1dd62eba8683ba7215c7e110ab0b000cd
Michael Treanor <skeptycal@gmail.com>
2022-01-21 17:13:53 -0600

    (GoBot) devtag version v0.1.0-1642806833464042

--- idea.md
+++ idea.md
@@ -4,4 +4,7 @@
 
 ## Goals
 
--
+-   maintain a collection of strings.Builder objects
+-   return a Builder object when requested
+-   release a Builder object when work is complete
+-   lower the workload on the GC
--- stringpool_test.go
+++ stringpool_test.go
@@ -17,17 +17,31 @@
 package stringpool
 
 import (
-	"fmt"
 	"reflect"
 	"strconv"
 	"strings"
 	"testing"
 )
 
+const (
+	// scaling factor (in powers of 2)
+	defaultMaxScalingFactor = 6
+	maxScalingFactor        = 10
+)
+
 type pooler interface {
 	Get() *strings.Builder
 	Release(sb *strings.Builder)
 }
+
+var (
+	sb         *strings.Builder = &strings.Builder{}
+	NewPool                     = New()
+	out        string           = "" // global string return value
+	global_n   int              = 0
+	global_err error            = nil
+	k          byte
+)
 
 type swimmer struct{ strings.Builder }
 
@@ -41,122 +55,6 @@ func (t swimmer) Release(sb *strings.Builder) {
 
 func sbNonPool() pooler {
 	return &swimmer{}
-}
-
-var (
-	sb         *strings.Builder = &strings.Builder{}
-	NewPool                     = New()
-	out        string           = "" // global string return value
-	global_n   int              = 0
-	global_err error            = nil
-	k          byte
-)
-
-func chr(c byte) string {
-	return fmt.Sprintf("%c", c)
-}
-
-const (
-	// scaling factor (in powers of 2)
-	defaultMaxScalingFactor = 6
-	maxScalingFactor        = 10
-)
-
-type (
-	Any interface{}
-
-	GetSetter interface {
-		Get(key Any) (Any, error)
-		Set(key Any, value Any) error
-	}
-
-	// Args implements a map of arguments
-	Args interface {
-		GetSetter
-	}
-
-	args map[Any]Any
-
-	ArgSet []Args
-
-	Benchmark interface{}
-
-	Benchmarks interface {
-		Name() string
-		Scale() int
-		Count() int
-		Next() Benchmark
-		Setup() setupFunc
-		Cleanup() cleanupFunc
-	}
-
-	benchmark struct {
-		name    string   // name of benchmark test
-		argSets []ArgSet // multiple runs with multiple argSets
-		want    Any      // return value wanted
-		wantErr bool     // is error wanted?
-	}
-
-	benchmarkSet struct {
-		name          string      // name of benchmark set
-		scale         int         // current scaling factor
-		counter       int         // current trial counter
-		runs          []benchmark // multiple runs if multiple Arg sets
-		scalingFactor int         // max scaling factor for benchmark set (1-10)
-		setup         setupFunc   // function used to setup benchmarks
-		cleanup       cleanupFunc // function used to cleanup benchmarks
-	}
-
-	setupFunc   = func(set *benchmarkSet) error
-	cleanupFunc = func(set *benchmarkSet) error
-)
-
-func NewBenchmarkSet(name string, tests []benchmark, scalingFactor int, setup setupFunc, cleanup cleanupFunc) *benchmarkSet {
-	if scalingFactor < 1 || scalingFactor > maxScalingFactor {
-		scalingFactor = defaultMaxScalingFactor
-	}
-
-	if setup == nil {
-		setup = defaultSetup
-	}
-
-	if cleanup == nil {
-		cleanup = defaultCleanup
-	}
-	return &benchmarkSet{
-		name:          name,
-		scale:         0,
-		counter:       0,
-		runs:          tests,
-		scalingFactor: scalingFactor,
-		setup:         setup,
-		cleanup:       cleanup,
-	}
-}
-
-func BenchmarkAll(b *testing.B) {
-	benchmarks := benchmarkSet{
-		name:          "",
-		scale:         0,
-		counter:       0,
-		runs:          []benchmark{},
-		scalingFactor: defaultMaxScalingFactor,
-		setup:         func(set *benchmarkSet) error { return nil },
-		cleanup:       func(set *benchmarkSet) error { return nil },
-	}
-
-}
-
-func defaultSetup(set *benchmarkSet) error {
-
-	// set any global variables here
-	// pass configuration options using default _config
-
-	return nil
-}
-
-func defaultCleanup() error {
-	return nil
 }
 
 func BenchmarkStringPool(b *testing.B) {
